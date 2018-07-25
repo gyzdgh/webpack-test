30599414ab81dc8a5c353b0d933f8b00efabb965
高远 <gyz@192.168.2.108>
2018-07-25 16:59:15 +0800

    add save_todo

--- task-4/main.js
+++ task-4/main.js
@@ -11,8 +11,8 @@ import Vue from 'vue'
 import AV from 'leancloud-storage'
 
 //初始化数据库
-var APP_ID = '8axnRtGoxCJhEzsvNPEAHnol-gzGzoHsz';
-var APP_KEY = '0YH4XkYflb4CUPfA743TGj8G';
+var APP_ID = 'NjlgdBY3hHhl5vs5mqXSzeXu-gzGzoHsz';
+var APP_KEY = 'jKWke83lEgGz3qEyw9jauWHb';
 AV.init({
   appId: APP_ID,
   appKey: APP_KEY
@@ -46,18 +46,73 @@ var app = new Vue({
   },
   //将待办保存在 localStorage 中
   created: function () {
-    window.onbeforeunload = () => {
-      let dataString = JSON.stringify(this.todoList)
-      window.localStorage.setItem('myTodos', dataString)
-    }
-
-    let oldDataString = window.localStorage.getItem('myTodos')
-    let oldData = JSON.parse(oldDataString)
-    this.todoList = oldData || []
-    //检查用户是否登陆
+    // //当窗口即将被关闭的时候触发事件
+    // window.onbeforeunload = () => {
+    //   //指定值对应的JSON字符串
+    //   let dataString = JSON.stringify(this.todoList)
+    //   //设置当前页面的 localStorage 
+    //   window.localStorage.setItem('myTodos', dataString)
+    // }
+    // //读取上一次的 localStorage
+    // let oldDataString = window.localStorage.getItem('myTodos')
+    // //用JSON解析
+    // let oldData = JSON.parse(oldDataString)
+    // //设置待办项的内容
+    // this.todoList = oldData || []
+    // //检查用户是否登陆
     this.currentUser = this.getCurrentUser();
+    this.fetchTodos() // 将原来的一坨代码取一个名字叫做 fetchTodos
   },
   methods: {
+    fetchTodos: function () {
+      //判断是否为当前用户
+      if (this.currentUser) {
+        //获取 User 的 AllTodos
+        var query = new AV.Query('AllTodos');
+        query.find()
+          .then((todos) => {
+            let avAllTodos = todos[0] // 因为理论上 AllTodos 只有一个，所以我们取结果的第一项
+            //保存id
+            let id = avAllTodos.id
+            this.todoList = JSON.parse(avAllTodos.attributes.content) // 为什么有个 attributes？因为我从控制台看到的
+            this.todoList.id = id // 为什么给 todoList 这个数组设置 id？因为数组也是对象啊
+          }, function (error) {
+            console.error(error)
+          })
+      }
+    },
+    updateTodos: function () {
+      let dataString = JSON.stringify(this.todoList)
+      let avTodos = AV.Object.createWithoutData('AllTodos', this.todoList.id)
+      avTodos.set('content', dataString)
+      avTodos.save().then(() => {
+        console.log('更新成功')
+      })
+    },
+    saveTodos: function () {
+      let dataString = JSON.stringify(this.todoList)
+      var AVTodos = AV.Object.extend('AllTodos');
+      var avTodos = new AVTodos();
+      var acl = new AV.ACL()
+      acl.setReadAccess(AV.User.current(), true) // 只有这个 user 能读
+      acl.setWriteAccess(AV.User.current(), true) // 只有这个 user 能写
+
+      avTodos.set('content', dataString);
+      avTodos.setACL(acl) // 设置访问控制
+      avTodos.save().then((todo) => {
+        this.todoList.id = todo.id  // 一定要记得把 id 挂到 this.todoList 上，否则下次就不会调用 updateTodos 了
+        console.log('保存成功');
+      }, function (error) {
+        alert('保存失败');
+      });
+    },
+    saveOrUpdateTodos: function () {
+      if (this.todoList.id) {
+        this.updateTodos()
+      } else {
+        this.saveTodos()
+      }
+    },
     //添加待办
     addTodo: function () {
       this.todoList.push({
@@ -67,33 +122,42 @@ var app = new Vue({
       })
       //变为空
       this.newTodo = ''
+      this.saveOrUpdateTodos()
     },
     // 加了👇这个函数，删除待办
     removeTodo: function (todo) {
       let index = this.todoList.indexOf(todo)
       this.todoList.splice(index, 1)
+      this.saveOrUpdateTodos()
     },
     //注册功能
     signUp: function () {
+      //声明一个新的用户
       let user = new AV.User();
       user.setUsername(this.formData.username);
       user.setPassword(this.formData.password);
+      //成功注册之后，执行的逻辑
       user.signUp().then((loginedUser) => {    // 👈，改成箭头函数方便用this
         this.currentUser = this.getCurrentUser()
-      }, function (error) {
+      }, (error) => {
+        //异常处理
         alert('注册失败')
+        console.log(error)
       });
     },
     //登入功能
     login: function () {
       AV.User.logIn(this.formData.username, this.formData.password).then((loginedUser) => {// 👈，改成箭头函数方便用this
         this.currentUser = this.getCurrentUser()
+        this.fetchTodos() // 登录成功后读取 todos
       }, function (error) {
         alert('登录失败')
+        console.log(error)
       });
     },
     //获取当前用户
     getCurrentUser: function () {
+      // LeanCloud 文档说 AV.User.current() 可以获取当前登录的用户
       let current = AV.User.current()
       if (current) {
         //获取用户的ID，用户名，创建时间
